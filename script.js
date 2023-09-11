9b07a70cf69e422767d82d0bd88d71d4ec5ce83e
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-09-11 23:37:23 +0100

    Add error message and styles

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ const button = document.querySelector('#submit');
 
 button.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log('clicked');
   const formData = inputField.value;
-  if (formData ==='') {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const wrongFormat = () => {
     inputField.style.borderColor = 'red'
     errorMssg.innerHTML = 'Please provide a valid email address';
     errorMssg.style.fontSize = '10px';
     errorMssg.style.color = 'red';
     errorMssg.style.fontStyle = 'italic';
     errorMssg.style.fontFamily = 'inherit';
+    return false;
   }
+
+  if (formData ==='') {
+    wrongFormat();
+  } else if (!emailRegex.test(formData)) {
+    wrongFormat();
+  } else {
+    location.reload();
+  }
+  
 })
